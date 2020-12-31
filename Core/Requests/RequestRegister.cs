b9466c325f95a75e20d96cf39e1e297a2623c71a
Breno Miranda <brenomirandaster@gmail.com>
2020-12-31 01:02:34 -0300

    adding core -> RequestRegister

---
+++ Core/Requests/RequestRegister.cs
@@ -0,0 +1,8 @@
+namespace Backend.Core.Request
+{
+    public class RequestRegister : BaseRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
