b4230cafcff3000a8710d57aca3cfe7e93c6866d
Breno Miranda <brenomirandaster@gmail.com>
2020-12-31 01:12:40 -0300

    adding to MySqlContext

---
+++ Models/MySqlContext.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public partial class NetCoreAuthJwtMySqlContext : DbContext
+    {
+        public NetCoreAuthJwtMySqlContext()
+        {
+        }
+
+        public NetCoreAuthJwtMySqlContext(DbContextOptions<NetCoreAuthJwtMySqlContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;User=root;password=senha;port=3306;database=jetecommerce;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasColumnType("varchar(50)")
+                    .HasDefaultValueSql("'0'");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasColumnType("varchar(128)")
+                    .HasDefaultValueSql("'0'");
+
+                entity.Property(e => e.Role)
+                    .IsRequired()
+                    .HasColumnName("role")
+                    .HasColumnType("varchar(20)")
+                    .HasDefaultValueSql("'0'");
+
+                entity.Property(e => e.Salt)
+                    .IsRequired()
+                    .HasColumnName("salt")
+                    .HasColumnType("varchar(36)")
+                    .HasDefaultValueSql("'0'");
+            });
+        }
+    }
+}
